aa901ed65557dbf3d99ecaf6f34954acdfbbed9b
Marcin Gasperowicz <xnooga@gmail.com>
2026-04-19 20:06:31 +0100

    Update Homebrew formula to v1.5.0

--- HomebrewFormula/let-go.rb
+++ HomebrewFormula/let-go.rb
@@ -5,27 +5,27 @@ class LetGo < Formula
   desc "A Clojure dialect implemented as a bytecode VM in Go"
   homepage "https://github.com/nooga/let-go"
   license "MIT"
-  version "1.4.0"
+  version "1.5.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/nooga/let-go/releases/download/v1.4.0/let-go_1.4.0_darwin_amd64.tar.gz"
-      sha256 "73cdd393d5c6ac27bf2e6af3235918879d23d93a3519449d479e511d3db60fa0"
+      url "https://github.com/nooga/let-go/releases/download/v1.5.0/let-go_1.5.0_darwin_amd64.tar.gz"
+      sha256 "1da75fe6d50834930ec22f64476a1707f81bdfb0db38e87ad447494957607484"
     end
     on_arm do
-      url "https://github.com/nooga/let-go/releases/download/v1.4.0/let-go_1.4.0_darwin_arm64.tar.gz"
-      sha256 "a3c71ec0fd6a7df4f2cf6b19fec3d09a6b8709c028c353515fdd3f503d94c826"
+      url "https://github.com/nooga/let-go/releases/download/v1.5.0/let-go_1.5.0_darwin_arm64.tar.gz"
+      sha256 "e87502b4d4dca1715ff360e837c83df88819ccdad069fa77f8aeedb86776342f"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/nooga/let-go/releases/download/v1.4.0/let-go_1.4.0_linux_amd64.tar.gz"
-      sha256 "657f7ab72f57ce9a3939a77651f6b379648332df67c5c70b8247d5fa7b6b192b"
+      url "https://github.com/nooga/let-go/releases/download/v1.5.0/let-go_1.5.0_linux_amd64.tar.gz"
+      sha256 "49cc2f3fb6156d443a978e8532c302d5066163029506de98f5315f57d719b3d2"
     end
     on_arm do
-      url "https://github.com/nooga/let-go/releases/download/v1.4.0/let-go_1.4.0_linux_arm64.tar.gz"
-      sha256 "1ba8f02e074104393daf4fb0482aeb79d575dcdf67efebd458272630c379484b"
+      url "https://github.com/nooga/let-go/releases/download/v1.5.0/let-go_1.5.0_linux_arm64.tar.gz"
+      sha256 "8d281dbad80f717e5c3e0ea0a53da9007c0c27893d77f52e1dadc4915ab20e9c"
     end
   end
 
